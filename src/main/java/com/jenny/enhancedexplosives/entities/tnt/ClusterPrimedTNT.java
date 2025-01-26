2aaecd966822fd655496c42436857ac3710e7037
Jenny <JennyTSonline@proton.me>
2025-01-26 20:44:19 +0100

    fix claymore tnt entity texture

--- src/main/java/com/jenny/enhancedexplosives/entities/tnt/ClusterPrimedTNT.java
+++ src/main/java/com/jenny/enhancedexplosives/entities/tnt/ClusterPrimedTNT.java
@@ -1,9 +1,12 @@
 package com.jenny.enhancedexplosives.entities.tnt;
 
+import com.jenny.enhancedexplosives.blocks.blocks;
 import com.jenny.enhancedexplosives.entities.entities;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
@@ -17,5 +20,10 @@ public class ClusterPrimedTNT extends basePrimedTNT {
 
     public ClusterPrimedTNT(EntityType<ClusterPrimedTNT> entityType, Level level) {
         super(entityType, level, null);
+    }
+
+    @Override
+    public Block renderBlock() {
+        return Blocks.TNT;
     }
 }
