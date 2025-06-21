2e2b7d0d222b0720a11a1b9c47ec027fa4dd715e
Jenny <jennytsonline@proton.me>
2025-06-21 16:54:01 +0200

    fix crash on dedicated server

--- src/main/java/com/jenny/enhancedexplosives/ClientEvents.java
+++ src/main/java/com/jenny/enhancedexplosives/ClientEvents.java
@@ -1,16 +1,19 @@
 package com.jenny.enhancedexplosives;
 
+import com.jenny.enhancedexplosives.entities.entities;
 import com.jenny.enhancedexplosives.particles.ArrowParticle;
 import com.jenny.enhancedexplosives.particles.particles;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import static com.jenny.enhancedexplosives.EnhancedExplosives.MODID;
 
-@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class eventBusEvents {
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ClientEvents {
     @SubscribeEvent
     public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
         Minecraft.getInstance().particleEngine.register(particles.CONCUSSIVE_ARROW_PARTICLE.get(),
@@ -21,5 +24,10 @@ public class eventBusEvents {
                 ArrowParticle.Provider::new);
         Minecraft.getInstance().particleEngine.register(particles.TUNNEL_ARROW_PARTICLE.get(),
                 ArrowParticle.Provider::new);
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        entities.registerRenderers();
     }
 }
